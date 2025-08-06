5af7e66ca23a80c51ec7c0e564f09605bad65222
nisargapatilk <user@DESKTOP-5636MPU>
2025-08-06 14:48:03 +0530

    Dashoard-Report

---
+++ HarmanKnowledgeHubPortal.Domain/Repositories/IDashboardAndReportsRepository.cs
@@ -0,0 +1,16 @@
+﻿using HarmanKnowledgeHubPortal.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HarmanKnowledgeHubPortal.Domain.Repositories
+{
+    public interface IDashboardAndReportsRepository
+    {
+        Task<Report> GetDashboardByUserIdAsync(int userId);
+        Task<List<Report>> GetReportsAsync(int userId, DateTime? startDate, DateTime? endDate, string category);
+        Task CreateReportAsync(Report report);
+        Task UpdateReportAsync(Report report);
+        Task SoftDeleteReportAsync(int id);
+    }
+}
---
+++ HarmanKnowledgeHubPortal.Domain/Services/DashboardService.cs
@@ -0,0 +1,30 @@
+﻿using HarmanKnowledgeHubPortal.Domain.Entities;
+using HarmanKnowledgeHubPortal.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarmanKnowledgeHubPortal.Domain.Services
+{
+    public class DashboardService
+    {
+        private readonly IDashboardAndReportsRepository _dashboardRepo;
+
+        public DashboardService(IDashboardAndReportsRepository dashboardRepo)
+        {
+            _dashboardRepo = dashboardRepo;
+        }
+
+        public Report GetDashboard(int userId)
+        {
+            return _dashboardRepo.GetDashboardByUserId(userId);
+        }
+
+        public List<Report> GetReports(int userId, DateTime? start, DateTime? end, string category)
+        {
+            return _dashboardRepo.GetReports(userId, start, end, category);
+        }
+    }
+}
