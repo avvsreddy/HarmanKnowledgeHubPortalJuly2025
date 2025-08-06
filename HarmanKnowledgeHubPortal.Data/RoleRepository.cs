4ab5ebcc8d6a0c068a2faba78f18e674043ec316
Lalli17 <yaswanthlalitha@gmail.com>
2025-08-06 22:58:17 +0530

    DTO_SERVICES updated

---
+++ HarmanKnowledgeHubPortal.Data/RoleRepository.cs
@@ -0,0 +1,31 @@
+﻿using HarmanKnowledgeHubPortal.Domain.Entities;
+using HarmanKnowledgeHubPortal.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarmanKnowledgeHubPortal.Data
+{
+        public class RoleRepository : IRoleRepository
+        {
+            private readonly AppDbContext _context;
+
+            public RoleRepository(AppDbContext context)
+            {
+                _context = context;
+            }
+
+            public Role GetByName(string roleName)
+            {
+                return _context.Roles.FirstOrDefault(r => r.Name == roleName);
+            }
+
+            public List<Role> GetAll()
+            {
+                return _context.Roles.ToList();
+            }
+        }
+    }
+
---
+++ HarmanKnowledgeHubPortal.Domain/DTO/ReviewArticleDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarmanKnowledgeHubPortal.Domain.DTO
+{
+    public class ReviewArticleDto
+    {
+        [Required]
+        public List<int> ArticleIds { get; set; }
+
+        [Required]
+        public string Action { get; set; } // "Approve" or "Reject"
+    }
+}
