41e8be07806f1a925b453cfd09bf848ef3ed1154
Ujwal-Khairnar <user@DESKTOP-5636MPU>
2025-08-11 08:19:35 +0530

    fetched current changes

--- HarmanKnowledgeHubPortal.Domain/DTO/BrowseUrlDTO.cs
+++ HarmanKnowledgeHubPortal.Domain/DTO/BrowseUrlDTO.cs
@@ -10,7 +10,7 @@ namespace HarmanKnowledgeHubPortal.Domain.DTO
     {
         public string Title { get; set; }
         public string Url { get; set; }
-        public string Description { get; set; }
+        public string Description { get; set; }      //review,reports
         public string PostedBy { get; set; }        // e.g., User Name or Email
         public string CategoryName { get; set; }    // e.g., "Programming", "Design"
     }
